502fe71f463ca310e179d0f2042736d7920798a1
Carlos Guzman <eumir.cg@gmail.com>
2023-01-29 11:17:27 -0500

    contacts api started

--- patients/src/api/contacts.py
+++ patients/src/api/contacts.py
@@ -1,3 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, abort
+from ..models import Patient, Contacts_Rx, db
 
 bp = Blueprint('contacts', __name__, url_prefix='/contacts')
+
+@bp.route('', methods=['POST'])
+def create():
+    if 'patients_id' not in request.json or 'lens_brand' not in request.json:
+        return abort(400)
+    Patient.query.get_or_404(request.json['patients_id'])
+    c = Contacts_Rx(
+        exp_date=request.json['exp_date'],
+        valid=request.json['valid'],
+        lens_brand=request.json['lens_brand'],
+        od_rx=request.json['od_rx'],
+        os_rx=request.json['os_rx'],
+        pres_doc=request.json['pres_doc'],
+        patient_id=request.json['patients_id']
+    )
+    db.session.add(c)
+    db.session.commit()
+    return jsonify(c.serialize())
+
+@bp.route('', methods=['GET'])
+def index():
+    contacts = Contacts_Rx.query.all()
+    result = []
+    for c in contacts:
+        result.append(c.serialize())
+    return jsonify(result)
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    c = Contacts_Rx.query.get_or_404(id)
+    return jsonify(c.serialize())
--- patients/src/api/glasses.py
+++ patients/src/api/glasses.py
@@ -5,16 +5,30 @@ bp = Blueprint('glasses', __name__, url_prefix='/glasses')
 
 @bp.route('', methods=['POST'])
 def create():
-    if 'patients_id' not in request.json or 'prescription' not in request.json:
+    if 'patients_id' not in request.json or 'od_rx' not in request.json or 'os_rx' not in request.json:
         return abort(400)
     Patient.query.get_or_404(request.json['patients_id'])    
     g = Glasses_Rx(
         exp_date=request.json['exp_date'],
         valid=request.json['valid'],
-        prescription=request.json['prescription'],
-        pres_od=request.json['pres_od'],
+        od_rx=request.json['od_rx'],
+        os_rx=request.json['os_rx'],
+        pres_doc=request.json['pres_doc'],
         patients_id=request.json['patients_id']
     )
     db.session.add(g)
     db.session.commit()
+    return jsonify(g.serialize())
+
+@bp.route('', methods=['GET'])
+def index():
+    glasses = Glasses_Rx.query.all()
+    result = []
+    for g in glasses:
+        result.append(g.serialize())
+    return jsonify(result)
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    g = Glasses_Rx.query.get_or_404(id)
     return jsonify(g.serialize())
